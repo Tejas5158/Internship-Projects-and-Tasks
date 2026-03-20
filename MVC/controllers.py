dd111c49dad1be80b11689bfa1b30c02ae7e939f
tejas5158 <kadamtejas9236@gmail.com>
2026-03-20 10:44:03 +0530

    controller.py file created

---
+++ MVC/controllers.py
@@ -0,0 +1,33 @@
+from sqlalchemy.orm import Session
+from models.user_model import User
+
+def create_user(db: Session, user):
+    db_user = User(name=user.name, email=user.email, password=user.password)
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
+
+def get_users(db: Session):
+    return db.query(User).all()
+
+def get_user(db: Session, user_id: int):
+    return db.query(User).filter(User.id == user_id).first()
+
+def update_user(db: Session, user_id: int, data):
+    user = db.query(User).filter(User.id == user_id).first()
+    if user:
+        if data.name:
+            user.name = data.name
+        if data.email:
+            user.email = data.email
+        db.commit()
+        db.refresh(user)
+    return user
+
+def delete_user(db: Session, user_id: int):
+    user = db.query(User).filter(User.id == user_id).first()
+    if user:
+        db.delete(user)
+        db.commit()
+    return user
