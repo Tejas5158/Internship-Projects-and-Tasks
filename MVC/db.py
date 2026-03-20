62417c648d0ab01c1d48f3367f8fadeea0c821d7
tejas5158 <kadamtejas9236@gmail.com>
2026-03-20 11:21:34 +0530

    model.py file created

--- MVC/db.py
+++ MVC/db.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "mysql+pymysql://root:root@localhost/tasks_db"
+DATABASE_URL = "mysql+pymysql://root:root@localhost/user_db"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine)
--- mvc_basic/model.py
+++ mvc_basic/model.py
@@ -1,10 +1,11 @@
 from sqlalchemy import Column, Integer, String
-from database.db import Base
+from sqlalchemy.orm import declarative_base
+
+Base = declarative_base()
 
 class User(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, index=True)
-    name = Column(String(100))
-    email = Column(String(100), unique=True)
-    password = Column(String(100))
+    name = Column(String(50))
+    email = Column(String(50))
